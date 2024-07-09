5a726716dbaf4509c47e346b3a82a5af3cb9b36f
NehemieMbg <nehemie.mbg@gmail.com>
2024-07-10 01:31:15 +0200

    Adds Server

---
+++ apps/server/src/test/java/_6nehemie/com/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package _6nehemie.com.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
